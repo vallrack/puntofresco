31facbd8d114735df8173d8bb711597c63a3d6ed
Danny Morales <programming2025dan@gmail.com>
2025-12-20 00:37:05 +0000

    habia olvidado darte esto:
    // Import the functions you need from the SD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,13 @@
 // Replace this with your own Firebase config object.
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 export const firebaseConfig = {
-  apiKey: "AIzaSyDrz-67scpfn0iGGocWOTgASJmcCdfp4c4",
+  apiKey: "AIzaSyCAKZLQAIzD91SYANU-BF2AVESJ9YLVlgI",
   authDomain: "punto-fresco-f0c35.firebaseapp.com",
   projectId: "punto-fresco-f0c35",
   storageBucket: "punto-fresco-f0c35.appspot.com",
-  messagingSenderId: "657652328756",
-  appId: "1:657652328756:web:4f6de2023eac26b2ce9d61",
+  messagingSenderId: "971416463936",
+  appId: "1:971416463936:web:1cd12041fa7338b125cda5",
+  measurementId: "G-SG4MBEHDLD"
 };
 
 // This function is for internal tooling only and should not be used.
